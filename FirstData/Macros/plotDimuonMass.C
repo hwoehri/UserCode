093b215b2eae59919bfbcda8ceb6da75a8ad91fd
hwoehri <>
2010-01-09 16:09:40 +0000

    included the LS muon pairs

--- FirstData/Macros/plotDimuonMass.C
+++ FirstData/Macros/plotDimuonMass.C
@@ -5,8 +5,8 @@
 #include "TLatex.h"
 
 #include "commonVar.inc"
-
-TH1F *hReco_mass[kNbCath][kNbCuts];
+Int_t colour[kNbCharge] = {1,2,3,2};
+TH1F *hReco_mass[kNbCharge][kNbCath][kNbCuts];
 Char_t *oniaLabel[3] = {"global-global", "global-tracker", "tracker-tracker"};
 
 void LoadData(Char_t *fileNameIn);
@@ -33,14 +33,15 @@ void PlotData(Char_t *enLabel){
     c1[iCut]->Divide(3,1);
     for(int iCat = 0; iCat < 3; iCat++){
       c1[iCut]->cd(iCat+1);
-      hReco_mass[iCat][iCut]->SetMaximum(2.*hReco_mass[iCat][iCut]->GetMaximum());
-      hReco_mass[iCat][iCut]->Draw("hist");
-      tex1[iCat][iCut] = new TLatex(3.3, 0.8*hReco_mass[iCat][iCut]->GetMaximum(), oniaLabel[iCat]);
+      hReco_mass[0][iCat][iCut]->SetMaximum(2.*hReco_mass[0][iCat][iCut]->GetMaximum());
+      hReco_mass[0][iCat][iCut]->Draw("hist");
+      hReco_mass[3][iCat][iCut]->Draw("histsame");
+      tex1[iCat][iCut] = new TLatex(3.3, 0.8*hReco_mass[0][iCat][iCut]->GetMaximum(), oniaLabel[iCat]);
       tex1[iCat][iCut]->SetTextSize(0.05);
       tex1[iCat][iCut]->Draw();
       sprintf(name, "%s GeV", enLabel);
-      tex1[iCat][iCut]->DrawLatex(3.3, 0.87*hReco_mass[iCat][iCut]->GetMaximum(), name);
-      tex1[iCat][iCut]->DrawLatex(3.3, 0.7*hReco_mass[iCat][iCut]->GetMaximum(), muCutName[iCut]);
+      tex1[iCat][iCut]->DrawLatex(3.3, 0.87*hReco_mass[0][iCat][iCut]->GetMaximum(), name);
+      tex1[iCat][iCut]->DrawLatex(3.3, 0.7*hReco_mass[0][iCat][iCut]->GetMaximum(), muCutName[iCut]);
     }
     sprintf(name, "Figures/dimuMass_%sGeV_%s.gif", muCutName[iCut], enLabel);
     c1[iCut]->Print(name);
@@ -51,13 +52,16 @@ void LoadData(Char_t *fileNameIn){
 
   TFile *fIn = new TFile(fileNameIn, "r");
   Char_t name[100];
-  for(int iCat = 0; iCat < 3; iCat++){
-    for(int iCut = 0; iCut < kNbCuts; iCut++){
-      sprintf(name, "hReco_mass_%s_%s_%s", chargeName[0], oniaCatName[iCat], muCutName[iCut]);
-      hReco_mass[iCat][iCut] = (TH1F *) gDirectory->Get(name);
-      hReco_mass[iCat][iCut]->Rebin(5);
-      hReco_mass[iCat][iCut]->SetYTitle("dN/dM [per 50 MeV/c^{2}]");
-      hReco_mass[iCat][iCut]->SetAxisRange(1., 5.);
+  for(int iCharge = 0; iCharge < kNbCharge; iCharge++){
+    for(int iCat = 0; iCat < 3; iCat++){
+      for(int iCut = 0; iCut < kNbCuts; iCut++){
+	sprintf(name, "hReco_mass_%s_%s_%s", chargeName[iCharge], oniaCatName[iCat], muCutName[iCut]);
+	hReco_mass[iCharge][iCat][iCut] = (TH1F *) gDirectory->Get(name);
+	hReco_mass[iCharge][iCat][iCut]->Rebin(5);
+	hReco_mass[iCharge][iCat][iCut]->SetYTitle("dN/dM [per 50 MeV/c^{2}]");
+	hReco_mass[iCharge][iCat][iCut]->SetAxisRange(1., 5.);
+	hReco_mass[iCharge][iCat][iCut]->SetLineColor(colour[iCharge]);
+      }
     }
   }
 }
