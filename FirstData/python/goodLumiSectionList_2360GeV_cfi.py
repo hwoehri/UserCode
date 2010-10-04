a54e944116f0c13e37dd35a737c3b7dc1f3feab7
hwoehri <>
2010-10-04 12:04:15 +0000

    This commit was manufactured by cvs2svn to create tag 'V04-10-10'.
    Sprout from master 2010-10-04 12:04:14 UTC hwoehri 'replaced JpsiMassMin and -Max by nSigMass, polMassJpsi and sigmaMassJpsi'
    Delete:
        FirstData/Macros/ProjectQQ.C
        FirstData/Macros/ProjectQQ.h
        FirstData/Macros/commonVar.inc
        FirstData/Macros/plotDimuonMass.C
        FirstData/Macros/plotSignalAndBGofMC.C
        FirstData/Macros/plotSingleMuVariables.C
        FirstData/Macros/runProjectQQ.C
        FirstData/python/goodLumiSectionList_2360GeV_cfi.py
        FirstData/python/goodLumiSectionList_900GeV_cfi.py
        FirstData/python/jpsi_data09_2360GeV_cfg.py
        FirstData/python/jpsi_data09_900GeV_cfg.py
        FirstData/python/skimData09_allSignComb_cfg.py
        FirstData/python/skimData09_cfg.py

--- FirstData/python/goodLumiSectionList_2360GeV_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# filter on lumisections
-goodLumisToProcess = cms.untracked.VLuminosityBlockRange('124120:2-124120:58',
-                                                         '124275:2-124275:31')
