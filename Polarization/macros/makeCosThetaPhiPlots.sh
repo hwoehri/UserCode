730337ebc1248c6cd01fb641e3ad1f0cb25c51a1
hwoehri <>
2010-10-25 13:40:56 +0000

    plot cosTheta vs phi (2D and 1D projections) for different triggers

---
+++ Polarization/macros/makeCosThetaPhiPlots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+thisFrame=0
+while [ $thisFrame -lt 2 ]; do
+
+    thisPT=1
+    while [ $thisPT -lt 13 ]; do 
+
+	thisRap=1
+	while [ $thisRap -lt 5 ]; do 
+
+	    root -b -q "plotRec.C+(${thisFrame}, ${thisPT}, ${thisRap})"
+	    thisRap=$(( $thisRap + 1 ))
+	done
+	thisPT=$(( $thisPT + 1 ))
+    done
+    thisFrame=$(( $thisFrame + 1 ))
+done
