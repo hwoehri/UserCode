286cc874eaea372613a82c6171316796952361dd
hwoehri <>
2010-11-29 13:56:49 +0000

    replaced TLorentzVector with Double_t's

--- Generation/MakeGenTree/src/MakeGenTree.cc
+++ Generation/MakeGenTree/src/MakeGenTree.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Hermine Woehri,40 1-A32,+41227679747,
 //         Created:  Thu Nov 25 15:20:12 CET 2010
-// $Id$
+// $Id: MakeGenTree.cc,v 1.1 2010/11/25 14:34:56 hwoehri Exp $
 //
 //
 
@@ -34,7 +34,7 @@
 
 #include <TFile.h>
 #include <TTree.h>
-#include <TLorentzVector.h>
+//#include <TLorentzVector.h>
 //
 // class declaration
 //
@@ -61,7 +61,11 @@ private:
   string outputFileName_;
   TFile *fOut_;
   TTree *tree_;
-  TLorentzVector *jPsi_4mom_, *muPos_4mom_, *muNeg_4mom_;
+  Double_t jPsi_Px, jPsi_Py, jPsi_Pz;
+  Double_t muPos_Px, muPos_Py, muPos_Pz;
+  Double_t muNeg_Px, muNeg_Py, muNeg_Pz;
+
+  //  TLorentzVector *jPsi_4mom_, *muPos_4mom_, *muNeg_4mom_;
 };
 
 //
@@ -89,9 +93,9 @@ MakeGenTree::~MakeGenTree()
  
    // do anything here that needs to be done at desctruction time
    // (e.g. close files, deallocate resources etc.)
-  delete jPsi_4mom_;
-  delete muPos_4mom_;
-  delete muNeg_4mom_;  
+//   delete jPsi_4mom_;
+//   delete muPos_4mom_;
+//   delete muNeg_4mom_;  
 
   fOut_->Close();
 //   delete tree_;
@@ -117,13 +121,28 @@ MakeGenTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    for (HepMC::GenEvent::particle_const_iterator it = genEvent->particles_begin(); it != genEvent->particles_end(); ++it){
      HepMC::FourVector momP = (*it)->momentum();
      if((*it)->pdg_id() == 13){//neg. muon
-       muNeg_4mom_->SetPxPyPzE(momP.px(), momP.py(), momP.pz(), momP.e());
+       muNeg_Px = momP.px();
+       muNeg_Py = momP.py();
+       muNeg_Pz = momP.pz();
+
+//        muNeg_4mom_->SetPxPyPzE(momP.px(), momP.py(), momP.pz(), momP.e());
      }
      else if((*it)->pdg_id() == -13){//pos. muon
-       muPos_4mom_->SetPxPyPzE(momP.px(), momP.py(), momP.pz(), momP.e());
+
+       muPos_Px = momP.px();
+       muPos_Py = momP.py();
+       muPos_Pz = momP.pz();
+//        muPos_4mom_->SetPxPyPzE(momP.px(), momP.py(), momP.pz(), momP.e());
      }
      else if ((*it)->pdg_id() == QQbarPDG_){//quarkonium
-       jPsi_4mom_->SetPxPyPzE(momP.px(), momP.py(), momP.pz(), momP.e());
+
+       jPsi_Px = momP.px();
+       jPsi_Py = momP.py();
+       jPsi_Pz = momP.pz();
+
+//        jPsi_4mom_->SetPxPyPzE(momP.px(), momP.py(), momP.pz(), momP.e());
+
+
 //        HepMC::ThreeVector prodVtx = (*it)->production_vertex()->point3d();
 //        originQQbar_->SetXYZ(prodVtx.x(), prodVtx.y(), prodVtx.z());
 //        cout << "origin: " << prodVtx.x() << " " << prodVtx.y() <<  " " << prodVtx.z() << endl;
@@ -140,10 +159,13 @@ MakeGenTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    tree_->Fill();
 
    //reset the variables for the next event
-   jPsi_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
-   muPos_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
-   muNeg_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
+//    jPsi_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
+//    muPos_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
+//    muNeg_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
 
+   jPsi_Px = -9999; jPsi_Py = -9999; jPsi_Pz = -9999; 
+   muPos_Px = -9999; muPos_Py = -9999; muPos_Pz = -9999; 
+   muNeg_Px = -9999; muNeg_Py = -9999; muNeg_Pz = -9999; 
 }
 
 // ------------ method called once each job just before starting event loop  ------------
@@ -152,21 +174,34 @@ MakeGenTree::beginJob()
 {
   fOut_ = new TFile(outputFileName_.c_str(), "RECREATE");
 
-  jPsi_4mom_ = new TLorentzVector();
-  muPos_4mom_ = new TLorentzVector();
-  muNeg_4mom_ = new TLorentzVector();
+//   jPsi_4mom_ = new TLorentzVector();
+//   muPos_4mom_ = new TLorentzVector();
+//   muNeg_4mom_ = new TLorentzVector();
 
   tree_ = new TTree("QQbarGenTree", "generator level info for acceptance calculation");
 
   //single muon variables:
-  tree_->Branch("jPsi_4mom", "TLorentzVector", &jPsi_4mom_);
-  tree_->Branch("muPos_4mom", "TLorentzVector", &muPos_4mom_);
-  tree_->Branch("muNeg_4mom", "TLorentzVector", &muNeg_4mom_);
+//   tree_->Branch("jPsi_4mom", "TLorentzVector", &jPsi_4mom_);
+//   tree_->Branch("muPos_4mom", "TLorentzVector", &muPos_4mom_);
+//   tree_->Branch("muNeg_4mom", "TLorentzVector", &muNeg_4mom_);
 
    //set default values
-   jPsi_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
-   muPos_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
-   muNeg_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
+//    jPsi_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
+//    muPos_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
+//    muNeg_4mom_->SetPxPyPzE(-9999, -9999, -9999, -9999);
+
+  //jPsi:
+  tree_->Branch("jPsi_Px", &jPsi_Px, "jPsi_Px/D");
+  tree_->Branch("jPsi_Py", &jPsi_Py, "jPsi_Py/D");
+  tree_->Branch("jPsi_Pz", &jPsi_Pz, "jPsi_Pz/D");
+  //muPos
+  tree_->Branch("muPos_Px", &muPos_Px, "muPos_Px/D");
+  tree_->Branch("muPos_Py", &muPos_Py, "muPos_Py/D");
+  tree_->Branch("muPos_Pz", &muPos_Pz, "muPos_Pz/D");
+  //muNeg
+  tree_->Branch("muNeg_Px", &muNeg_Px, "muNeg_Px/D");
+  tree_->Branch("muNeg_Py", &muNeg_Py, "muNeg_Py/D");
+  tree_->Branch("muNeg_Pz", &muNeg_Pz, "muNeg_Pz/D");
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
