2e60757bf29ae6824527d61c14f761252b855123
hwoehri <>
2011-05-04 20:14:55 +0000

    *** empty log message ***

--- Efficiencies/macros/plotSingleMuEff.C
+++ Efficiencies/macros/plotSingleMuEff.C
@@ -4,14 +4,12 @@
 
 Int_t const kNbEff = 7;
 Char_t *effFileNames[kNbEff] = {"/Users/hwoehri/CMS/Work/TnP/LucaPerrozzi/17March2011/MuonTrackingEff_17March2011.root",
-				"/Users/hwoehri/CMS/Work/TnP/Zongchang/29March2011/MuonIDEff_29March2011.root",
-				"/Users/hwoehri/CMS/Work/TnP/Xianyou/25March2011/MuonQualEff_25March2011.root",
-				//"/Users/hwoehri/CMS/Work/TnP/Francesco/2April2011/L1L2_DMu0_TriggerEfficiencies_RUNB_distM2gt120_2April2011_fitted.root",
-  				"/Users/hwoehri/CMS/Work/TnP/Francesco/2April2011/L1L2_DMu0_TriggerEfficiencies_RUNA_distM2gt120_2April2011_fitted.root",
-  				// "/Users/hwoehri/CMS/Work/TnP/Luigi/5April2011/L3_DoubleMu0_TriggerEfficiencies_Run2010A_distM1gt150_4April2011_fitted.root",
-  				"/Users/hwoehri/CMS/Work/TnP/Luigi/5April2011/L3_DoubleMu0_TriggerEfficiencies_Run2010B_distM1gt150_4April2011_fitted.root",
-//  				"/Users/hwoehri/CMS/Work/TnP/Luigi/4April2011/L3_DoubleMu0_TriggerEfficiencies_Run2010A_cutDistM1_4April2011_fitted.root",
-// 				"/Users/hwoehri/CMS/Work/TnP/Luigi/4April2011/L3_DoubleMu0_TriggerEfficiencies_Run2010B_cutDistM1_4April2011_fitted.root",
+				"/Users/hwoehri/CMS/Work/TnP/Zongchang/29March2011/MuonIDEff_29March2011_fitted.root",
+				"/Users/hwoehri/CMS/Work/TnP/Xianyou/25March2011/MuonQualEff_25March2011_fitted.root",
+				"/Users/hwoehri/CMS/Work/TnP/Francesco/2April2011/L1L2_DMu0_TriggerEfficiencies_RUNB_distM2gt120_7April2011_fitted.root",
+  				//"/Users/hwoehri/CMS/Work/TnP/Francesco/2April2011/L1L2_DMu0_TriggerEfficiencies_RUNA_distM2gt120_7April2011_fitted.root",
+				"/Users/hwoehri/CMS/Work/TnP/Luigi/5April2011/L3_DoubleMu0_TriggerEfficiencies_Run2010A_distM1gt150_7April2011_fitted.root",
+				//"/Users/hwoehri/CMS/Work/TnP/Luigi/5April2011/L3_DoubleMu0_TriggerEfficiencies_Run2010B_distM1gt150_7April2011_fitted.root",
 // 				"/Users/hwoehri/CMS/Work/TnP/Herbert/28March2011/HLT_Track_Mu0TkMu0_Run1.root",
  				"/Users/hwoehri/CMS/Work/TnP/Herbert/29March2011/HLT_Track_Mu0TkMu0_Run_Tightv3.root",
 // 				"/Users/hwoehri/CMS/Work/TnP/Ilse/29March2011/HLTMuonTrack_Mu0_TkMu0_TM_runA_29March2011.root"};
@@ -52,7 +50,8 @@ Int_t const colourEta[kNbPT] = {1,2,3,4,kYellow-6,6,7,8,9,10,11};
 //Int_t const colourPT[kNbEta] = {1,2,3,4,kYellow-6,6,7,8};
 Int_t const colourPT[kNbEta] = {1,2,4};
 //Float_t binsEta[kNbEta] = {1.2, 1.6,  2.4}; //Track-TkMu0
-Float_t binsEta[kNbEta] = {0.2, 0.3, 0.6, 0.8, 1.2, 1.5, 2.1, 2.4};
+//Float_t binsEta[kNbEta] = {0.2, 0.3, 0.6, 0.8, 1.2, 1.5, 2.1, 2.4};
+Float_t binsEta[kNbEta] = {0.2, 0.3, 0.6, 0.8, 1.2, 1.6, 2.1, 2.4};
 //Float_t binsPT[kNbPT+1] = {0.7, 1., 1.25, 1.5, 1.75, 2., 2.5, 3.5, 4., 6., 10., 20., 30.};
 Float_t binsPT[kNbPT+1] = {0.8, 1.25, 1.5, 1.75, 2., 2.5, 3.3, 4., 6., 10., 20., 30.};
 void LoadEfficiencies(Int_t iEff, Int_t iEffSample);
@@ -60,40 +59,39 @@ void PlotEff_DataVsMC_PT(Int_t iEff, Int_t iEta);
 void PlotEff_DataVsMC_Eta(Int_t iEff, Int_t iPT);
 Double_t erfunction(Double_t *x, Double_t *par);
 Double_t erfunction_corr(Double_t *x, Double_t *par);
-void ParametrizePT(Int_t iEff, Int_t iEffSample, Int_t iEta, Bool_t plateauCorrection);
+void ParametrizePT(Int_t iEff, Int_t iEffSample, Int_t iEta, Bool_t saveFit, Bool_t plateauCorrection);
 //==============================================================
-void plotSingleMuEff(Bool_t fitAndSave = kFALSE,
+void plotSingleMuEff(Bool_t saveFit = kFALSE,
 		     Bool_t plateauCorrection = kTRUE){
 
-//   for(int iEff = 0; iEff < kNbEff; iEff++){
-//   for(int iEff = 1; iEff < 2; iEff++){//MuonID efficiency (Zongchang)
-//   for(int iEff = 2; iEff < 3; iEff++){//Muon Quality efficiency (Xianyou)
-  // for(int iEff = 3; iEff < 4; iEff++){//L1/L2 trigger efficiency (Francesco)
-   for(int iEff = 4; iEff < 5; iEff++){//L3 trigger efficiency (Luigi)
-//   for(int iEff = 5; iEff < 6; iEff++){//Tracking trigger eff. of TkMu0 (Herbert)
-//   for(int iEff = 6; iEff < 7; iEff++){//Muon trigger eff. of TkMu0 (Ilse)
+  //for(int iEff = 0; iEff < kNbEff; iEff++){
+  //for(int iEff = 1; iEff < 2; iEff++){//MuonID efficiency (Zongchang)
+  //for(int iEff = 2; iEff < 3; iEff++){//Muon Quality efficiency (Xianyou)
+  //for(int iEff = 3; iEff < 4; iEff++){//L1/L2 trigger efficiency (Francesco)
+  for(int iEff = 4; iEff < 5; iEff++){//L3 trigger efficiency (Luigi)
+    //for(int iEff = 5; iEff < 6; iEff++){//Tracking trigger eff. of TkMu0 (Herbert)
+    //for(int iEff = 6; iEff < 7; iEff++){//Muon trigger eff. of TkMu0 (Ilse)
     for(int iEffSample = 0; iEffSample < kNbEffSample; iEffSample++) //DATA, MC, MCTRUTH
-//     for(int iEffSample = 0; iEffSample < 2; iEffSample++) //DATA, MC, MCTRUTH
       LoadEfficiencies(iEff, iEffSample);
 
-    Int_t maxEta = kNbEta;
+    // Int_t maxEta = kNbEta;
+    // for(int iEta = 0; iEta < maxEta; iEta++){
+    //   PlotEff_DataVsMC_PT(iEff, iEta);
+    
+    fitPT = new TF1("fitPT", erfunction, 0., 100., 3);
+    fitPT->SetParName(0, "xPos-Offset");
+    fitPT->SetParName(1, "xPos-Plateau");
+    fitPT->SetParName(2, "yPos-Plateau");
+    fitPT_PlateauCorrected = new TF1("fitPT_PlateauCorrected", erfunction_corr, 0., 100., 3);
+    fitPT_PlateauCorrected->SetParName(0, "xPos-Offset");
+    fitPT_PlateauCorrected->SetParName(1, "xPos-Plateau");
+    fitPT_PlateauCorrected->SetParName(2, "yPos-Plateau");
 
-    if(fitAndSave){
+    for(int iEffSample = 0; iEffSample < 1; iEffSample++){
 
-      fitPT = new TF1("fitPT", erfunction, 0., 30., 3);
-      fitPT->SetParName(0, "xPos-Offset");
-      fitPT->SetParName(1, "xPos-Plateau");
-      fitPT->SetParName(2, "yPos-Plateau");
-      fitPT_PlateauCorrected = new TF1("fitPT_PlateauCorrected", erfunction_corr, 0., 30., 3);
-      fitPT_PlateauCorrected->SetParName(0, "xPos-Offset");
-      fitPT_PlateauCorrected->SetParName(1, "xPos-Plateau");
-      fitPT_PlateauCorrected->SetParName(2, "yPos-Plateau");
+      for(int iEta = 0; iEta < maxEta; iEta++){
+    	ParametrizePT(iEff, iEffSample, iEta, saveFit, plateauCorrection);
 
-      for(int iEffSample = 0; iEffSample < 1; iEffSample++){
-
-	for(int iEta = 0; iEta < maxEta; iEta++){
-	  ParametrizePT(iEff, iEffSample, iEta, plateauCorrection);
-	}
       }
     }
   }
@@ -111,8 +109,8 @@ void PlotEff_DataVsMC_PT(Int_t iEff, Int_t iEta){
   TH1F *hFrame1 = gPad->DrawFrame(0., minY, 31., maxY);
   hFrame1->SetXTitle("p_{T}(#mu) GeV");
   
-  gEff_pT[iEff][MC][iEta]->Draw("p same");   gEff_pT[iEff][MC][iEta]->SetLineColor(2); gEff_pT[iEff][MC][iEta]->SetMarkerColor(2);
-  gEff_pT[iEff][MCTRUTH][iEta]->Draw("p same"); gEff_pT[iEff][MCTRUTH][iEta]->SetLineColor(4); gEff_pT[iEff][MCTRUTH][iEta]->SetMarkerColor(4);
+  // gEff_pT[iEff][MC][iEta]->Draw("p same");   gEff_pT[iEff][MC][iEta]->SetLineColor(2); gEff_pT[iEff][MC][iEta]->SetMarkerColor(2);
+  // gEff_pT[iEff][MCTRUTH][iEta]->Draw("p same"); gEff_pT[iEff][MCTRUTH][iEta]->SetLineColor(4); gEff_pT[iEff][MCTRUTH][iEta]->SetMarkerColor(4);
   gEff_pT[iEff][DATA][iEta]->Draw("p same"); gEff_pT[iEff][DATA][iEta]->SetLineColor(1); gEff_pT[iEff][DATA][iEta]->SetMarkerColor(1);
 
   TLatex *tex1;
@@ -141,9 +139,8 @@ void PlotEff_DataVsMC_PT(Int_t iEff, Int_t iEta){
   }
   TLegend *leg = new TLegend(0.7456897,0.1610169,0.9454023,0.3622881, name);
   leg->AddEntry(gEff_pT[iEff][DATA][iEta], "Data T&P", "pl");
-  leg->AddEntry(gEff_pT[iEff][MC][iEta], "MC T&P", "pl");
-//   if(iEff != 6)
-  leg->AddEntry(gEff_pT[iEff][MCTRUTH][iEta], "MC Truth", "pl");
+  // leg->AddEntry(gEff_pT[iEff][MC][iEta], "MC T&P", "pl");
+  // leg->AddEntry(gEff_pT[iEff][MCTRUTH][iEta], "MC Truth", "pl");
   leg->SetFillColor(0); leg->SetTextSize(0.04); 
   leg->SetBorderSize(0); leg->Draw();
 
@@ -216,10 +213,10 @@ void LoadEfficiencies(Int_t iEff, Int_t iEffSample){
 
 
 //==========================================
-void ParametrizePT(Int_t iEff, Int_t iEffSample, Int_t iEta, Bool_t plateauCorrection){
+void ParametrizePT(Int_t iEff, Int_t iEffSample, Int_t iEta, Bool_t saveFit, Bool_t plateauCorrection){
 
   Char_t name[100];
-  sprintf(name, "c3_%s_pT_etaBin%d", effName[iEff], iEta);
+  sprintf(name, "c3_%s_%s_pT_etaBin%d", effName[iEff], effSampleName[iEffSample], iEta);
   TCanvas *c1 = new TCanvas(name, name);
   Double_t minY = 0, maxY = 1.2;
   if(iEff == 6){minY = 0.6, maxY = 1.1;}
@@ -229,37 +226,55 @@ void ParametrizePT(Int_t iEff, Int_t iEffSample, Int_t iEta, Bool_t plateauCorre
   gEff_pT[iEff][iEffSample][iEta]->Draw("p same"); gEff_pT[iEff][iEffSample][iEta]->SetLineColor(1); gEff_pT[iEff][iEffSample][iEta]->SetMarkerColor(1);
   // if((iEta == 1 || iEta == 3) || !plateauCorrection){
   if(!plateauCorrection){
-    fitPT->SetParameter(0, -0.6);
-    fitPT->SetParameter(1, 5.);
+    fitPT->SetParameter(0, -0.3);
+    fitPT->SetParameter(1, 2.);
     fitPT->SetParameter(2, -0.01);
-    if(iEta > 5)
+    if(iEta > 4)
       gEff_pT[iEff][iEffSample][iEta]->Fit("fitPT", "", "", 0., 20.);
     else
       gEff_pT[iEff][iEffSample][iEta]->Fit("fitPT", "", "", 2., 20.);
   }
   else{
     fitPT_PlateauCorrected->SetParameter(0, -0.6);
-    fitPT_PlateauCorrected->SetParameter(1, 5.);
+    fitPT_PlateauCorrected->SetParameter(1, 7.);
     fitPT_PlateauCorrected->SetParameter(2, -0.01);
-    if(iEta > 5)
+    if(iEta > 4)
       gEff_pT[iEff][iEffSample][iEta]->Fit("fitPT_PlateauCorrected", "", "", 0., 20.);
-    else if(iEta == 3)
-      gEff_pT[iEff][iEffSample][iEta]->Fit("fitPT_PlateauCorrected", "", "", 2., 10.);
+    // else if(iEta == 3) //L3 trigger efficiency
+    //   gEff_pT[iEff][iEffSample][iEta]->Fit("fitPT_PlateauCorrected", "", "", 2., 10.);
     else
       gEff_pT[iEff][iEffSample][iEta]->Fit("fitPT_PlateauCorrected", "", "", 2., 20.);
   }
+
+  TF1 *fitFunc;
   
-  // TF1 *fitFunc;
-  // if(!plateauCorrection)
-  //   fitFunc = gEff_pT[iEff][iEffSample][iEta]->GetFunction("fitPT");
-  // else
-  //   fitFunc = gEff_pT[iEff][iEffSample][iEta]->GetFunction("fitPT_PlateauCorrected");
-  // sprintf(name, "fit%s_%s_pt_etaBin%d", effName[iEff], effSampleName[iEffSample], iEta);
-  // fitFunc->SetName(name);
-  // TFile *fOut = new TFile(effFileNames[iEff], "UPDATE");
-  // fitFunc->Write();
-  // fOut->Close();
+  if(!plateauCorrection){
+    sprintf(name, "fit%s_%s_pt_etaBin%d", effName[iEff], effSampleName[iEffSample], iEta);
+    fitFunc = new TF1(name, erfunction, 0., 100., 3);
+    fitPT = gEff_pT[iEff][iEffSample][iEta]->GetFunction("fitPT");
+    for(int iPar = 0; iPar < 3; iPar++)
+      fitFunc->FixParameter(iPar, fitPT->GetParameter(iPar));
+  }
+  else{
+    sprintf(name, "fit%s_%s_pt_etaBin%d", effName[iEff], effSampleName[iEffSample], iEta);
+    fitFunc = new TF1(name, erfunction_corr, 0., 100., 3);
+    fitPT_PlateauCorrected = gEff_pT[iEff][iEffSample][iEta]->GetFunction("fitPT_PlateauCorrected");
+    for(int iPar = 0; iPar < 3; iPar++){
+      fitFunc->FixParameter(iPar, fitPT_PlateauCorrected->GetParameter(iPar));
+      printf("setting parameter %d to %1.3f\n", iPar, fitPT_PlateauCorrected->GetParameter(iPar));
+    }
+  }
+  fitFunc->SetName(name);
+  fitFunc->SetLineWidth(1);
+  fitFunc->SetLineColor(2);
+  fitFunc->Draw("same");
+  //fitFunc->SetRange(0., 100.);
   
+  if(saveFit){
+    TFile *fOut = new TFile(effFileNames[iEff], "UPDATE");
+    fitFunc->Write();
+    fOut->Close();
+  }
 
   TLatex *tex1;
   if(iEff == 6) 
@@ -283,7 +298,7 @@ void ParametrizePT(Int_t iEff, Int_t iEffSample, Int_t iEta, Bool_t plateauCorre
       sprintf(name, "1.6 < |#eta(#mu)| < 2.4");
   }
 
-  TLine *line1 = new TLine(0., 1., 31., 1.);
+  TLine *line1 = new TLine(0., 1., 30., 1.);
   line1->SetLineStyle(3); line1->Draw();
 
   TLegend *leg = new TLegend(0.7456897,0.2610169,0.9454023,0.3622881, name);
@@ -300,6 +315,11 @@ Double_t erfunction(Double_t *x, Double_t *par){
 
   Double_t value;
   value = par[2] + TMath::Erf(par[0] + x[0]/ par[1]);
+  if(value > 1.) 
+    return 1.;
+  // if(value < 0.)
+  //   return 0.;
+
   return value;
 }
 
@@ -307,15 +327,20 @@ Double_t erfunction(Double_t *x, Double_t *par){
 Double_t erfunction_corr(Double_t *x, Double_t *par){
 
   Double_t value;
-  value = par[2] + TMath::Erf(par[0] + x[0]/ par[1]);
+  value = par[2] + TMath::Erf(par[0] + x[0]/par[1]);
 
-  if(value < 0.99 && x[0] > par[1] && x[0] < 20.){
-    Double_t k = (value-1) / (par[1] - 20.);
-    Double_t d = 1.-20.*(value -1.)/(par[1]-20.);
+  if(value < 1.0 && x[0] > (par[1]+par[0]) && x[0] < 20.){
+    Double_t k = (value-1.) / ((par[1]+par[0]) - 20.);
+    Double_t d = 1.-20.*k;
     value = k*x[0] + d;
   }
   else if(x[0] > 20.)
     value = 1.;
+
+  if(value < 0.)
+    value = 0.;
+  // if(value > 1.)
+  //   value = 1.;
 
   return value;
 }
