d2b5d84fdcd4468859027ee75a6b53cc834c6b47
hwoehri <>
2011-06-25 10:54:24 +0000

    updated to new y, pT definitions

--- Efficiencies/interface/commonVar.h
+++ Efficiencies/interface/commonVar.h
@@ -14,17 +14,28 @@ namespace eff{
   //rap bins
   Int_t const kNbRapForPTBins = 5;
   /* Double_t rapForPTRange[kNbRapForPTBins+1] = {0., 0.9, 1.5, 1.9, 2.3}; */
-  Double_t rapForPTRange[kNbRapForPTBins+1] = {0., 0.9, 1.2, 1.6, 2.1, 2.4};
+  /* Double_t rapForPTRange[kNbRapForPTBins+1] = {0., 0.9, 1.2, 1.6, 2.1, 2.4}; */
+  Double_t rapForPTRange[kNbRapForPTBins+1] = {0., 0.5, 1.0, 1.5, 2.0, 2.4}; //21June 2011
   //pT bins (optimized to have at least 10.000 entries per bin)
-  Int_t const kNbPTMaxBins = 12;
-  Int_t const kNbPTBins[kNbRapForPTBins+1] = {kNbPTMaxBins, 7,8,9,12,12};//all y, y1, y2, y3, y4, y5
+  /* Int_t const kNbPTMaxBins = 12; */
+  /* Int_t const kNbPTBins[kNbRapForPTBins+1] = {kNbPTMaxBins, 7,8,9,12,12};//all y, y1, y2, y3, y4, y5 */
+  /* Double_t pTRange[kNbRapForPTBins+1][kNbPTMaxBins+1] = { */
+  /*   {0., 1., 2., 3., 4., 5., 6., 7., 8., 10., 15., 20., 30.},//all rapidities */
+  /*   {0., 6., 7., 8., 10., 15., 20., 30.},//mid-rap */
+  /*   {0., 4., 6., 7., 8., 10., 15., 20., 30.}, */
+  /*   {0., 4., 5., 6., 7., 8., 10., 15., 20., 30.}, */
+  /*   {0., 1., 2., 3., 4., 5., 6., 7., 8., 10., 15., 20., 30.}, */
+  /*   {0., 1., 2., 3., 4., 5., 6., 7., 8., 10., 15., 20., 30.}};//most forward */
+  //The following bins were tuned on 21 June 2011 to equalize the statistics
+  Int_t const kNbPTMaxBins = 19;
+  Int_t const kNbPTBins[kNbRapForPTBins+1] = {kNbPTMaxBins, 9,10,12,14,14};//all y, y1, y2, y3, y4, y5
   Double_t pTRange[kNbRapForPTBins+1][kNbPTMaxBins+1] = {
-    {0., 1., 2., 3., 4., 5., 6., 7., 8., 10., 15., 20., 30.},//all rapidities
-    {0., 6., 7., 8., 10., 15., 20., 30.},//mid-rap
-    {0., 4., 6., 7., 8., 10., 15., 20., 30.},
-    {0., 4., 5., 6., 7., 8., 10., 15., 20., 30.},
-    {0., 1., 2., 3., 4., 5., 6., 7., 8., 10., 15., 20., 30.},
-    {0., 1., 2., 3., 4., 5., 6., 7., 8., 10., 15., 20., 30.}};//most forward
+    {1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 18., 20., 25., 30.},//all rapidities
+    {                    6., 7., 8., 9., 10., 12., 15., 20., 25., 30.},//mid-rap
+    {4.,                 6., 7., 8., 9., 10., 12., 15., 20., 25., 30.},
+    {3., 4., 5.,         6., 7., 8., 9., 10., 12., 15., 20., 25., 30.},
+    {1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 12., 15., 20., 25., 30.},
+    {1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 12., 15., 20., 25., 30.}};//most forward
   //the following values are dummy and need to be filled out at end of analysis
   Double_t pTWCentre_rap[kNbRapForPTBins+1][23] = 
     {{0.5, 1.25, 1.75, 1.95, 2.5, 2.8, 3.1, 3.4, 3.9, 4.3, 4.6, 5.0, 5.5, 6.0, 6.5, 7.2, 7.8, 8.5, 9.6, 11.0, 14., 27.},
@@ -39,7 +50,6 @@ namespace eff{
   Char_t *frameLabel[kNbFrames] = {"CS", "HX", "PHX", "sGJ", "GJ1", "GJ2"};
 /*   Int_t const kNbFrames = 2; */
 /*   Char_t *frameLabel[kNbFrames] = {"CS", "HX"}; */
-
 
   //cosTheta 
   Int_t const kNbBinsCosT = 10;
@@ -70,9 +80,11 @@ namespace eff{
   //study the negative and positive rapidity sides separately
   Int_t const kNbRapBins = kNbRapForPTBins;
   /* Double_t rapRange[2*kNbRapBins+1] = {-2.3, -1.9, -1.5, -0.9, 0., 0.9, 1.5, 1.9, 2.3}; */
-  Double_t rapRange[2*kNbRapBins+1] = {-2.4, -2.1, -1.6, -1.2, -0.9, 0., 0.9, 1.2, 1.6, 2.1, 2.4};
+  /* Double_t rapRange[2*kNbRapBins+1] = {-2.4, -2.1, -1.6, -1.2, -0.9, 0., 0.9, 1.2, 1.6, 2.1, 2.4}; */
+  Double_t rapRange[2*kNbRapBins+1] = {-2.4, -2.0, -1.5, -1.0, -0.5, 0., 0.5, 1.0, 1.5, 2.0, 2.4};
   
   //phase space limiting cuts:
+  Double_t rapMax = 2.4;
   Int_t const kNbEtaRegions = 3;
   Double_t etaPS[kNbEtaRegions] = {1.3, 2.2, 2.4}; //pseudo-rap cuts for muons
   Double_t pTMuMin[kNbEtaRegions] = {3.3, 0., 0.8};
