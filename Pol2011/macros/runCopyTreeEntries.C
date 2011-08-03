0975643e8788a4f072207de7fc54707442d70233
hwoehri <>
2011-08-03 19:43:53 +0000

    adjusting the first pT index

--- Pol2011/macros/runCopyTreeEntries.C
+++ Pol2011/macros/runCopyTreeEntries.C
@@ -2,13 +2,13 @@
 #include "../interface/commonVar.h"
 
 //====================================
-void runCopyTreeEntries(Char_t *fileNameIn = "RootFiles/selEvents_data_Ups_2Aug2011.root"){
+void runCopyTreeEntries(Char_t *fileNameIn = "RootFiles/selEvents_data_Ups_noCowboys_3Aug2011.root"){
 
   gROOT->ProcessLine(".L CopyTreeEntries.C+");
 
   for(int iRap = 1; iRap <= 2; iRap++){
     Int_t max = onia::kNbPTBins[iRap]+1;
-    for(int iPT = 0; iPT < max; iPT++){
+    for(int iPT = 1; iPT < max; iPT++){
       CopyTreeEntries(iRap, iPT, fileNameIn);
     }
   }
