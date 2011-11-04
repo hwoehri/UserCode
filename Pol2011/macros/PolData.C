c43678e7de9fd0e6fca52f1b8bc298dfdf3e97f5
hwoehri <>
2011-11-04 14:45:25 +0000

    added all triggers (J/psi, psi', Upsilon) for the full 2011 data taking

--- Pol2011/macros/PolData.C
+++ Pol2011/macros/PolData.C
@@ -11,7 +11,7 @@
 //some statistics
 TH1F *Reco_StatEv;
 TH1F *Reco_Onia_mass[onia::kNbPTMaxBins+1][onia::kNbRapForPTBins+1];
-TH2F *Reco_Onia_rap_pT;
+TH2F *Reco_Onia_rap_pT, *Reco_Onia_rap_pT_noKinCuts;
 TH1F *Reco_Onia_pt[onia::kNbRapForPTBins+1];
 TH1F *Reco_Onia_rap[onia::kNbPTMaxBins+1];
 TTree *treeOut;
@@ -34,7 +34,7 @@ void PolData::Loop(Int_t selDimuType, Bool_t rejectCowboys)
 
   //loop over the events
   for (Long64_t jentry=0; jentry<nentries;jentry++) {
-  //for (Long64_t jentry=0; jentry<1000000;jentry++) {
+    //for (Long64_t jentry=0; jentry<1000000;jentry++) {
 
     if(jentry % 100000 == 0) printf("event %d\n", (Int_t) jentry);
 
@@ -58,14 +58,49 @@ void PolData::Loop(Int_t selDimuType, Bool_t rejectCowboys)
     //for the run ranges per HLT path during unprescaled running periods
 
     Int_t trigDecision = -99;
-    
-    if(//HLT_DoubleMu3_Quarkonium_v1 == 1 ||   //  5e32: 160404 - 161176
-       //HLT_DoubleMu3_Upsilon_v1 == 1 ||      //  5e32: 161216 - 163261
-       //HLT_Dimuon0_Barrel_Upsilon_v1 == 1 || //  5e32: 163269 - 163869
-       HLT_Dimuon5_Upsilon_Barrel_v1 == 1 || //  1e33: 165088 - 166967 and 1.4E33: 167039 - 167043
-       HLT_Dimuon5_Upsilon_Barrel_v2 == 1 || //  1e33: 166346
-       HLT_Dimuon5_Upsilon_Barrel_v3 == 1)   //1.4e33: 167078 - 167913
+
+    //Upsilon trigger paths:
+    // if(//HLT_DoubleMu3_Quarkonium_v1 == 1 ||   //  5e32: 160404 - 161176
+    //    //HLT_DoubleMu3_Upsilon_v1 == 1 ||      //  5e32: 161216 - 163261
+    //    //HLT_Dimuon0_Barrel_Upsilon_v1 == 1 || //  5e32: 163269 - 163869
+    //    HLT_Dimuon5_Upsilon_Barrel_v1 == 1 || //  1e33: 165088 - 166967 and 1.4E33: 167039 - 167043
+    //    HLT_Dimuon5_Upsilon_Barrel_v2 == 1 || //  1e33: 166346
+    //    HLT_Dimuon5_Upsilon_Barrel_v3 == 1 ||  //1.4e33: 167078 - 167913 (prescale of 2)
+    //    HLT_Dimuon5_Upsilon_Barrel_v5 == 1 || //2E33 (no cowboys)
+    //    HLT_Dimuon7_Upsilon_Barrel_v1 == 1 || //3E33 (L1_DoubleMu0_HighQ; becomes inactive for Linst >= 5E33)
+    //    HLT_Dimuon9_Upsilon_Barrel_v1 == 1 || //3E33 (L1_DoubleMu0_HighQ)
+    //    HLT_Dimuon7_Upsilon_Barrel_v4 == 1 || //5E33 (becomes inactive for Linst >= 5E33)
+    //    HLT_Dimuon9_Upsilon_Barrel_v4 == 1) //5E33
+    //    trigDecision = 1;
+
+    //Jpsi trigger paths
+    if(//HLT_DoubleMu3_Jpsi_v1 == 1 || // 5e32: 160404 - 161176
+       //HLT_DoubleMu3_Jpsi_v2 == 1 || // 5e32: 161216 - 163261
+       //HLT_Dimuon6p5_Jpsi_v1 || //5e32: 163269 - 163869
+       HLT_Dimuon10_Jpsi_Barrel_v1 || //1e33: 165088 - 166967
+       HLT_Dimuon10_Jpsi_Barrel_v2 || //1e33: 166346
+       HLT_Dimuon10_Jpsi_Barrel_v3 || //1.4e33: 167078 - 167913 (prescale = 2)
+       HLT_Dimuon10_Jpsi_Barrel_v5 || //2e33: 170249 - 172868 (no cowboys)
+       HLT_Dimuon10_Jpsi_Barrel_v6 || //3e33: 173236 - 177053 (L1_DoubleMu0_HighQ; inactive for Linst >= 4E33)
+       HLT_Dimuon13_Jpsi_Barrel_v1 || //3e33: 173236 - 177053 (L1_DoubleMu0_HighQ)
+       HLT_Dimuon10_Jpsi_Barrel_v9 || //5e33: 178380 - end (inactive for Linst >= 4E33)
+       HLT_Dimuon13_Jpsi_Barrel_v4) //5e33: 178380 - end 
       trigDecision = 1;
+
+    //psi' trigger paths
+    if(//HLT_DoubleMu3_Jpsi_v1 == 1 || // 5e32: 160404 - 161176
+       //HLT_DoubleMu3_Jpsi_v2 == 1 || // 5e32: 161216 - 163261
+       //HLT_Dimuon6p5_Jpsi_v1 || //5e32: 163269 - 163869
+       HLT_Dimuon7_PsiPrime_v1 || //1e33: 165088 - 166967
+       HLT_Dimuon7_PsiPrime_v2 || //1e33: 166346
+       HLT_Dimuon7_PsiPrime_v3 || //1.4e33: 167078 - 167913 (prescale = 2)
+       HLT_Dimuon7_PsiPrime_v5 || //2e33: 170249 - 172868 (no cowboys)
+       HLT_Dimuon9_PsiPrime_v1 || //3e33: 173236 - 178380 (L1_DoubleMu0_HighQ; inactive for Linst >= 4E33)
+       HLT_Dimuon11_PsiPrime_v1 || //3e33: 173236 - 178380 (L1_DoubleMu0_HighQ)
+       HLT_Dimuon9_PsiPrime_v4 || //5e33: 178421 - end (inactive for Linst >= 4E33)
+       HLT_Dimuon11_PsiPrime_v4) //5e33: 178421 - end 
+      trigDecision = 1;
+
     if(trigDecision != 1)
       continue;
 
@@ -108,8 +143,8 @@ void PolData::Loop(Int_t selDimuType, Bool_t rejectCowboys)
       continue;
     Reco_StatEv->Fill(4.5);
 
-    if(onia_mass < 8.4 || onia_mass > 11.6) //all Upsilons triggered
-      continue;
+    // if(onia_mass < 8.4 || onia_mass > 11.6) //all Upsilons triggered
+    //   continue;
     // if(onia_mass < (9.45 - 2.*0.080) || onia_mass > (9.45 + 2.*0.080)) //Ups(1S) only
     //   continue;
 
@@ -118,19 +153,23 @@ void PolData::Loop(Int_t selDimuType, Bool_t rejectCowboys)
     //apply different fiducial cuts for the muon matched to the HLT leg or the TM leg
     //for simplicity we decided to apply the stronger cuts to the higher pT muon and not
     //strictly to the HLT muon
+
+    Reco_Onia_rap_pT_noKinCuts->Fill(onia_rap, onia_pt);
     Bool_t muonsInAcc = kFALSE;
     if(isMuonInAcceptance(LOOSE, pTMuPos, etaMuPos) && isMuonInAcceptance(LOOSE, pTMuNeg, etaMuNeg))
+    //if(isMuonInAcceptance(TIGHT, pTMuPos, etaMuPos) && isMuonInAcceptance(TIGHT, pTMuNeg, etaMuNeg))
       muonsInAcc = kTRUE;
     if(!muonsInAcc)
       continue;
 
-   Reco_StatEv->Fill(5.5);
+   Reco_StatEv->Fill(6.5);
 
-   //reject furthermore all events in which one of the muons has a pT smaller than 3 GeV/c
-   // if(pTMuPos < 3.0 || pTMuNeg < 3.0)
+   //reject furthermore all events in which one of the muons has a pT smaller than 2 GeV/c
+   //given that the efficiency triggers only go down to 2 GeV/c
+   // if(pTMuPos < 2.0 || pTMuNeg < 2.0)
    //   continue;
 
-   Reco_StatEv->Fill(6.5);
+   Reco_StatEv->Fill(7.5);
 
    //set up the pT and y indices
    Int_t rapForPTIndex = -1;
